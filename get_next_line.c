364c135855844fd060593463b8aebf5400851997
jadonhansen <jadongavhansen@gmail.com>
2019-06-24 16:56:37 +0200

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jhansen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/19 11:28:55 by jhansen           #+#    #+#             */
-/*   Updated: 2019/06/24 14:49:00 by jhansen          ###   ########.fr       */
+/*   Updated: 2019/06/24 16:50:43 by jhansen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,38 +83,31 @@ int		get_next_line(const int fd, char **line)
 int		main(int argc, char **argv)
 {
     int		fd;
-    char	**line;
+    char	*line;
     int		gnlret;
     int		linecount;
 
 	linecount = 0;
 	gnlret = 1;
 	if (argc != 2)
-	{
-		printf("%s\n", "Input a file name please\n");
-		return (0);
-	}
-	else
-	{
-		fd = open(argv[1], O_RDONLY);
-		if (fd < 0)
+		fd = 0;
+	if (fd < 0)
 		{
 			printf("Could not find file: %s\n", argv[1]);
 			return (0);
 		}
 		printf("Argc: %d\n", argc);
 		printf("File to open: %s\n", argv[1]);
-		line = ft_memalloc(sizeof(char **));
+		line = ft_memalloc(sizeof(char *));
 		printf("BUFF_SIZE: %d\n\n", BUFF_SIZE);
-		while ((gnlret = get_next_line(fd, line)) > 0)
+		while ((gnlret = get_next_line(fd, &line)) > 0)
 		{
-			printf("%s", *line);
+			printf("%s", line);
 			printf("%d\n", gnlret);
 			linecount++;
 		}
 		printf("\nLine count: %d\n", linecount);
 		printf("Finished\n\n");
 		close(fd);
-	}
 	return (0);
 }
